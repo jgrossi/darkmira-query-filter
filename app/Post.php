8225d480e3487588f048ed0e789130838a2f5b44
Junior Grossi <juniorgro@gmail.com>
2018-04-04 22:14:26 -0300

    Implement filterable trait

--- app/Post.php
+++ app/Post.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use App\Http\Filters\Filterable;
 use Corcel\Model\Post as CorcelPost;
 
 class Post extends CorcelPost
 {
+    use Filterable;
+
     /**
      * @var string
      */
